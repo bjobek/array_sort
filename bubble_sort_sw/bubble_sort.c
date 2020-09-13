32ec2a12c11f090e39ec11310bd79b6890a762cf
bjobek <bjornivar95@gmail.com>
2020-09-13 20:40:25 +0200

    Bubble Sort software implementation

---
+++ bubble_sort_sw/bubble_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void swap(int* lhs, int* rhs){
+int tmp;
+tmp = *lhs;
+*lhs = *rhs;
+*rhs = tmp;
+
+
+}
+
+
+int main() {
+
+    
+    int len = 10;
+    int iarr[] = {3,9,5,8,7,3,2,9,12,3};
+
+    for(int i = 0; i < len; ++i){
+
+        printf("%d ",iarr[i]);
+    }
+    printf("\n");
+    int tmp = 0;
+    while(tmp < len){
+
+        int tmp_len = len - tmp;
+
+        for(int i = 1; i < tmp_len; ++i){
+            if( iarr[i] < iarr[i-1]){
+                swap(iarr + i, iarr +(i-1));
+
+        }
+        }
+        ++tmp;
+    }
+
+    for(int i = 0; i < len; ++i){
+
+        printf("%d ",iarr[i]);
+    }
+    printf("\n");
+
+}
